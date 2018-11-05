8f67c4aaf7828fd8ac097610cedb8e56d0a1d52c
Dnipro1918 <Khomutov100686@gmail.com>
2018-11-05 21:19:02 +0200

    Initial commit

---
+++ View/View/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace View
+{
+    partial class BaseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddContact = new System.Windows.Forms.Button();
+            this.DeleteContact = new System.Windows.Forms.Button();
+            this.ChangeContact = new System.Windows.Forms.Button();
+            this.ListOfContacts = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // AddContact
+            // 
+            this.AddContact.Location = new System.Drawing.Point(62, 94);
+            this.AddContact.Name = "AddContact";
+            this.AddContact.Size = new System.Drawing.Size(75, 23);
+            this.AddContact.TabIndex = 0;
+            this.AddContact.Text = "Добавить";
+            this.AddContact.UseVisualStyleBackColor = true;
+            this.AddContact.Click += new System.EventHandler(this.AddContact_Click);
+            // 
+            // DeleteContact
+            // 
+            this.DeleteContact.Enabled = false;
+            this.DeleteContact.Location = new System.Drawing.Point(62, 152);
+            this.DeleteContact.Name = "DeleteContact";
+            this.DeleteContact.Size = new System.Drawing.Size(75, 23);
+            this.DeleteContact.TabIndex = 1;
+            this.DeleteContact.Text = "Удалить";
+            this.DeleteContact.UseVisualStyleBackColor = true;
+            // 
+            // ChangeContact
+            // 
+            this.ChangeContact.Enabled = false;
+            this.ChangeContact.Location = new System.Drawing.Point(62, 123);
+            this.ChangeContact.Name = "ChangeContact";
+            this.ChangeContact.Size = new System.Drawing.Size(75, 23);
+            this.ChangeContact.TabIndex = 2;
+            this.ChangeContact.Text = "Изменить";
+            this.ChangeContact.UseVisualStyleBackColor = true;
+            // 
+            // ListOfContacts
+            // 
+            this.ListOfContacts.FormattingEnabled = true;
+            this.ListOfContacts.Location = new System.Drawing.Point(185, 34);
+            this.ListOfContacts.Name = "ListOfContacts";
+            this.ListOfContacts.Size = new System.Drawing.Size(182, 264);
+            this.ListOfContacts.TabIndex = 3;
+            // 
+            // BaseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 404);
+            this.Controls.Add(this.ListOfContacts);
+            this.Controls.Add(this.ChangeContact);
+            this.Controls.Add(this.DeleteContact);
+            this.Controls.Add(this.AddContact);
+            this.Name = "BaseForm";
+            this.Text = "BaseForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddContact;
+        private System.Windows.Forms.Button DeleteContact;
+        private System.Windows.Forms.Button ChangeContact;
+        private System.Windows.Forms.ListBox ListOfContacts;
+    }
+}
+
---
+++ View/View/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace View
+{
+    public partial class BaseForm : Form
+    {
+        public BaseForm()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private readonly ApplicationContext context;
+        public BaseForm(ApplicationContext context)
+        {
+            this.context = context;
+            InitializeComponent();
+
+            AddContact.Click += (sender, args) => Invoke(AddContact_Click);
+        }
+
+        public new void Show()
+        {
+            context.MainForm = this;
+            Application.Run(context);
+        }
+
+        string ListContact
+        {
+            get
+            {
+                return ListOfContacts.Text;
+            }
+        }
+        public event Action AddContact_Click;
+
+        public void ShowError(string errorMessage)
+        {
+            lblError.Text = errorMessage;
+        }
+
+        private void Invoke(Action action)
+        {
+            action?.Invoke();
+        }
+    }
+}
